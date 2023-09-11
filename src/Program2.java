66f6ddde1fb34a4cbe90e61ad96469463d8ab255
Nemolite <g25092011@mail.ru>
2023-09-11 20:19:05 +0300

    Program2

---
+++ src/Program2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=0;i<=5;sum +=i++);
+        System.out.println(sum);
+    }
+
+}
