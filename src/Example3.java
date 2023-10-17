407ab241df5b91ed9343a1dc92efa266b5b746b3
Nemolite <g25092011@mail.ru>
2023-10-17 07:54:58 +0300

    file

--- src/Example3.java
+++ src/Example3.java
@@ -1,2 +1,16 @@
+import java.io.*;
 public class Example3 {
+    public static void main(String[] args) throws IOException {
+        int i;
+        FileInputStream fin = new FileInputStream("D:\\Java\\myproject\\untitled\\src\\text.txt");
+        do{
+            i = fin.read();
+            if (i!=-1){
+                System.out.println((char)i);
+            }
+        }while (i!=-1);
+
+        fin.close();
+
+    }
 }
